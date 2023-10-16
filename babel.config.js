6ae955de937dd38f8852fcad0fbc32d948a6968c
mmreza <1tapciran@gmail.com>
2023-10-16 16:12:44 +0330

    اصلاح آدرس عکس های اسپلش اسکرین

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,12 @@ module.exports = function (api) {
         },
       ],
       "expo-router/babel",
+      
+      // For reanimated in web, we should use @babel/plugin-proposal-export-namespace-from
+      '@babel/plugin-proposal-export-namespace-from',
+      
+      // according to expo docs, Reanimated plugin has to be the last item in the plugins array.
+      // https://docs.expo.dev/versions/latest/sdk/reanimated/
       "react-native-reanimated/plugin",
     ],
   };
