844a603694ef28194e0da905dd94d68cbe02684a
sajadgnf <sajadganjifar@gmail.com>
2023-10-22 16:05:19 +0330

    migrate project details page

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
+/** @type {import('expo/metro-config').DefaultConfig} */
 const { getDefaultConfig } = require("expo/metro-config");
-
 const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.resolverMainFields.unshift("sbmodern");
 defaultConfig.resolver.sourceExts.push("cjs");
 defaultConfig.resolver.sourceExts.push("mjs");
-
+defaultConfig.resolver.assetExts.push("css");
+defaultConfig.transformer.assetPlugins.push("expo-asset/tools/hashAssetFiles");
 module.exports = defaultConfig;
