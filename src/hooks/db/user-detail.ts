56365368aa2bfe9a0fb4b121c59deab76cbe7f97
mmreza <1tapciran@gmail.com>
2023-11-04 18:56:39 +0330

    Merge branch 'fix/tour-reservation'

---
+++ src/hooks/db/user-detail.ts
@@ -0,0 +1,23 @@
+import NetInfo from "@react-native-community/netinfo";
+import { useUserDetailLazyQuery } from "@src/gql/generated";
+import { setUserDetail } from "@src/slice/user-slice";
+import { useDispatch } from "react-redux";
+
+const useUserDetail = () => {
+  const dispatch = useDispatch();
+  const [_, { networkStatus, refetch }] = useUserDetailLazyQuery({
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const syncTable = () => {
+    NetInfo.fetch().then(({ isConnected }) => {
+      if (isConnected) {
+        refetch().then(({ data }) => dispatch(setUserDetail(data.userDetail)));
+      }
+    });
+  };
+
+  return { syncTable, networkStatus };
+};
+
+export default useUserDetail;
--- src/theme.tsx
+++ src/theme.tsx
@@ -2,7 +2,6 @@ import { createTheme } from "@rneui/themed";
 import { DefaultTheme } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Platform } from "react-native";
-import useIsRtl from "./hooks/localization";
 import { WIDTH } from "./constants";
 
 export const PRIMARY_COLOR = "#FF4332";
@@ -62,30 +61,32 @@ export const theme = isRtl =>
           <LinearGradient {...props} colors={["#e1e8ee", "#d1d8de", "#e1e8ee"]} />
         ),
       },
-      Text: (props, theme) => ({
-        style: {
-          fontSize: 14,
-          fontFamily: isRtl
-            ? "DanaNoEn"
-            : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-          fontWeight: "400",
-          direction: isRtl ? "rtl" : "ltr",
-        },
-        h1Style: {
-          fontSize: 18,
-          fontFamily: isRtl
-            ? "DanaNoEn"
-            : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-          fontWeight: "400",
-        },
-        h2Style: {
-          fontSize: 16,
-          fontFamily: isRtl
-            ? "DanaNoEn"
-            : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-          fontWeight: "400",
-        },
-      }),
+      Text: (props, theme) => {
+        return {
+          style: {
+            fontSize: 14,
+            fontFamily: isRtl
+              ? "DanaNoEn"
+              : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+            fontWeight: props.bold ? "bold" : "400",
+            direction: isRtl ? "rtl" : "ltr",
+          },
+          h1Style: {
+            fontSize: 18,
+            fontFamily: isRtl
+              ? "DanaNoEn"
+              : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+            fontWeight: "400",
+          },
+          h2Style: {
+            fontSize: 16,
+            fontFamily: isRtl
+              ? "DanaNoEn"
+              : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+            fontWeight: "400",
+          },
+        };
+      },
       Button: (props, theme) => {
         const { type, color, size } = props;
 
@@ -154,6 +155,12 @@ export const theme = isRtl =>
         },
       }),
       Input: {
+        labelStyle: {
+          marginBottom: 5,
+          fontFamily: isRtl
+            ? "DanaNoEn"
+            : '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+        },
         inputStyle: {
           borderWidth: 1,
           borderRadius: 12,
