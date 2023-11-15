e45ba8d073d16c893cb973fb17eadf31b88b0b49
mmreza <1tapciran@gmail.com>
2023-11-15 11:40:28 +0330

    بهبود لیست تور ها از دید تشکل

--- app/(stack)/tour/management/index.tsx
+++ app/(stack)/tour/management/index.tsx
@@ -1,12 +1,22 @@
 import Container from "@atoms/container";
 import WhiteSpace from "@atoms/white-space";
-import { Badge, Button, Card, Text, useTheme } from "@rneui/themed";
-import { AccommodationQueryType } from "@src/gql/generated";
+import { Badge, BottomSheet, Button, ButtonGroup, Card, Text, useTheme } from "@rneui/themed";
+import {
+  AccommodationQueryType,
+  MyNgoDetailQuery,
+  TourTourStatusStepChoices,
+} from "@src/gql/generated";
 import useMyNGOTable from "@src/hooks/db/ngo";
 import { StyleSheet, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import useTranslation from "@src/hooks/translation";
+import { getTourRequestStatusBadgeColor } from "@src/helper/tour";
+import { useState } from "react";
+import ComingSoon from "@modules/coming-soon";
 
 const TourManagement = () => {
+  const { tr } = useTranslation();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   const { get } = useMyNGOTable();
   const { theme } = useTheme();
 
@@ -26,26 +36,40 @@ const TourManagement = () => {
             {tour.title}
           </Card.Title>
           <Card.FeaturedTitle caption>
-            {(tour.destination as AccommodationQueryType).province},{" "}
-            {(tour.destination as AccommodationQueryType).city}
+            {(tour.destination as AccommodationQueryType)?.province ?? tr("Province")},{" "}
+            {(tour.destination as AccommodationQueryType)?.city ?? tr("City")}
           </Card.FeaturedTitle>
           <Card.FeaturedSubtitle numberOfLines={2} type="grey3">
             {tour.description}
           </Card.FeaturedSubtitle>
           <Container size={10} style={styles.footer}>
             <View style={styles.buttonContainer}>
-              <Button type="outline" color="secondary" size="sm">
+              <Button type="outline" color="secondary" size="sm" onPress={() => setIsVisible(true)}>
                 <Feather name="trash" size={12} color={theme.colors.secondary} />
               </Button>
-              <Button type="outline" color="secondary" size="sm">
+              <Button type="outline" color="secondary" size="sm" onPress={() => setIsVisible(true)}>
                 <Feather name="edit" size={12} color={theme.colors.secondary} />
               </Button>
             </View>
-            <Badge value={tour.statusStep} color="warning" type="outline" />
+            <Badge
+              value={tour.statusStep}
+              color={getTourRequestStatusBadgeColor(tour)}
+              type="outline"
+            />
           </Container>
           <WhiteSpace size={10} />
         </Card>
       ))}
+      <BottomSheet isVisible={isVisible} onBackdropPress={() => setIsVisible(false)}>
+        <Container>
+          <WhiteSpace size={10} />
+          <ComingSoon />
+          <WhiteSpace size={10} />
+        <Button size="sm" type="outline" color="secondary" onPress={() => setIsVisible(false)}>
+          {tr("ok")}
+        </Button>
+        </Container>
+      </BottomSheet>
     </View>
   );
 };
