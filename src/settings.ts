50e7f93ad39fa67a86e49bae96696e365df98244
mmreza <1tapciran@gmail.com>
2023-12-06 18:44:30 +0330

    v2.0.7

--- src/settings.ts
+++ src/settings.ts
@@ -3,4 +3,4 @@ export const BASE_URL = "https://api.hamafza-startup.ir";
 export const PAGE_SIZE = 10;
 export const API_URL = BASE_URL + "/graphql/";
 export const RERENDERING_INSPECTOR_IS_ACTIVE = false;
-export const APP_VERSION = "2.0.5";
+export const APP_VERSION = "2.0.7";
