449dded9c9181fbeca3f53384e03b88e2168d649
mmreza <1tapciran@gmail.com>
2023-12-30 16:47:13 +0330

    لیست ها اگه آیتم نداشته باشن خالی میشن

--- src/components/organisms/host-list/index.tsx
+++ src/components/organisms/host-list/index.tsx
@@ -23,51 +23,50 @@ function HostList() {
     },
   });
 
-  return (
-    <>
-      <Container>
-        <TitleWithAction
-          title={tr("Available hosts")}
-          actionTitle={tr("See All")}
-          onActionPress={() => router.push("/search")}
-        />
-      </Container>
+  if (data?.projectList?.data?.length)
+    return (
+      <>
+        <Container>
+          <TitleWithAction
+            title={tr("Available hosts")}
+            actionTitle={tr("See All")}
+            onActionPress={() => router.push("/search")}
+          />
+        </Container>
 
-      <WhiteSpace size={16} />
+        <WhiteSpace size={16} />
 
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={style.gap}
-        style={style.listContainer}>
-        <View style={style.spacer}></View>
-        {loading
-          ? [1, 2, 3, 4, 5, 6, 7, 8].map(i => (
-              <Skeleton
-                key={i}
-                animation="pulse"
-                width={328}
-                height={300}
-                style={{ borderRadius: 10 }}
-              />
-            ))
-          : data?.projectList?.data?.length
-          ? data.projectList.data?.map((project, index) => (
-              <View key={index}>
-                <HostCard
-                  id={project.id}
-                  name={project.name}
-                  price={(project.price * (100 - project.discount)) / 100}
-                  address={project.accommodation.address}
-                  avatarS3={project.accommodation.avatarS3}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={style.gap}
+          style={style.listContainer}>
+          <View style={style.spacer}></View>
+          {loading
+            ? [1, 2, 3, 4, 5, 6, 7, 8].map(i => (
+                <Skeleton
+                  key={i}
+                  animation="pulse"
+                  width={328}
+                  height={300}
+                  style={{ borderRadius: 10 }}
                 />
-              </View>
-            ))
-          : null}
-        <View style={style.spacer}></View>
-      </ScrollView>
-    </>
-  );
+              ))
+            : data.projectList.data?.map((project, index) => (
+                <View key={index}>
+                  <HostCard
+                    id={project.id}
+                    name={project.name}
+                    price={(project.price * (100 - project.discount)) / 100}
+                    address={project.accommodation.address}
+                    avatarS3={project.accommodation.avatarS3}
+                  />
+                </View>
+              ))}
+          <View style={style.spacer}></View>
+        </ScrollView>
+      </>
+    );
 }
 
 const style = StyleSheet.create({
