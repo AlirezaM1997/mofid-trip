8ec92a85436f169f365927c11a0c73bca307b13a
m.rahimi <rahimimohammadhosein98@gmail.com>
2024-02-18 14:16:33 +0330

    ایجاد تب نقشه بجای سرچ

--- app/search.tsx
+++ app/search.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { View } from "react-native";
 import { router } from "expo-router";
 import { RootState } from "@src/store";
 import Container from "@atoms/container";
 import WhiteSpace from "@atoms/white-space";
-import { View } from "react-native";
 import useTranslation from "@src/hooks/translation";
-import { CategoryEnum, setCategory } from "@src/slice/filter-slice";
 import { useDispatch, useSelector } from "react-redux";
 import TitleWithAction from "@modules/title-with-action";
 import SearchBar from "@src/components/modules/search-bar";
+import { CategoryEnum, setCategory } from "@src/slice/filter-slice";
 import SearchTourHorizontalList from "@organisms/search/tour/horizontalList";
 import SearchHostHorizontalList from "@organisms/search/host/horizontalList";
 import { useProjectListSearchQuery, useTourListSearchQuery } from "@src/gql/generated";
